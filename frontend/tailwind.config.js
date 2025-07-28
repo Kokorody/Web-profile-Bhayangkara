8290609a61298b48b3bb1e002f4700e2afdcf159
Kokorody <kmsmuhammadrafi@gmail.com>
2025-07-29 02:09:00 +0700

    tailwind/postcss init

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'reveal': 'reveal 0.8s forwards',
+      },
+      keyframes: {
+        reveal: {
+          '0%': { 
+            opacity: '0',
+            transform: 'translate3d(0, 100px, 0)',
+          },
+          '100%': { 
+            opacity: '1',
+            transform: 'translate3d(0, 0, 0)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
